658eaaf722b001f1d0c51fb3551aef5333d17f49
juliaenriquetto <cc22139@g.unicamp.br>
2023-09-20 09:21:26 -0300

    implementação de classes necessárias acessar o BD

---
+++ app/src/main/java/com/example/sabersenior/Service.kt
@@ -0,0 +1,26 @@
+package com.example.sabersenior
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.DELETE
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
+interface Service {
+    //CRUD
+
+    @GET("/api/usuario/get/")
+    fun getDog(): Call<List<Usuario>>
+
+    @GET("/api/usuario/getId/{id}")
+    fun selecionarUsuario(): Call<List<Usuario>>
+
+    @POST("/api/usuario/post")
+    fun incluirUsuario(id:Int, usuario:Usuario): Call<List<Usuario>>
+
+    @PUT("/api/usuario/put/{id}")
+    fun alterarUsuario(id:Int, usuario:Usuario): Call<List<Usuario>>
+
+    @DELETE("/api/usuario/delete/{id}")
+    fun excluirUsuario(id:Int, usuario:Usuario): Call<Usuario>
+}
