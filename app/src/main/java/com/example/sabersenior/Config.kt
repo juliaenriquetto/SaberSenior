e322ce6cb17a451ae712d0029cfc877ea504e2b2
Laura Nakamura <112010242+Lauranakamura@users.noreply.github.com>
2023-09-27 10:15:10 -0300

    Dialog Sobre nós

--- app/src/main/java/com/example/sabersenior/Config.kt
+++ app/src/main/java/com/example/sabersenior/Config.kt
@@ -1,11 +1,41 @@
 package com.example.sabersenior
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Config : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
+
+        //Alert Dialog Btn_SobreNos
+
+        val btnSobreNos = findViewById<Button>(R.id.btnSobre)
+
+        btnSobreNos.setOnClickListener()
+        {
+            val dilaogBinding = layoutInflater.inflate(R.layout.custom_allert_sobre_nos, null)
+
+            val myDialog = Dialog(this)
+            myDialog.setContentView(dilaogBinding)
+
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+
+            val SairSobre = dilaogBinding.findViewById<Button>(R.id.dialog_Sair)
+
+            SairSobre.setOnClickListener()
+            {
+                myDialog.dismiss()
+            }
+        }
+        
+
+
     }
 }
