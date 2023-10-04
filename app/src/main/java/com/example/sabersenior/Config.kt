5d739644e6295053f5e93ccebb7f5ea5313e6676
Laura Nakamura <112010242+Lauranakamura@users.noreply.github.com>
2023-10-04 12:16:52 -0300

    aaa

--- app/src/main/java/com/example/sabersenior/Config.kt
+++ app/src/main/java/com/example/sabersenior/Config.kt
@@ -6,38 +6,58 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class Config : AppCompatActivity() {
+
+    private var currentTheme = R.style.AppTheme_LightTheme // Tema padrão é o tema claro
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(currentTheme)
         setContentView(R.layout.activity_config)
 
-        //Alert Dialog Btn_SobreNos
-
+        // Alert Dialog Btn_SobreNos
         val btnSobreNos = findViewById<Button>(R.id.btnSobre)
 
-        btnSobreNos.setOnClickListener()
-        {
-            val dilaogBinding = layoutInflater.inflate(R.layout.custom_allert_sobre_nos, null)
+        btnSobreNos.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.custom_allert_sobre_nos, null)
+            val RadiusP = layoutInflater.inflate(R.layout.dialog_mudtema, null)
 
             val myDialog = Dialog(this)
-            myDialog.setContentView(dilaogBinding)
+            myDialog.setContentView(dialogBinding)
+
+            val radioGroup = RadiusP.findViewById<RadioGroup>(R.id.radioGroup)
+            val radioLight = RadiusP.findViewById<RadioButton>(R.id.radioLight)
+            val radioDark = RadiusP.findViewById<RadioButton>(R.id.radioDark)
+
+            // Configure os RadioButtons para refletir o tema atual
+            if (currentTheme == R.style.AppTheme_LightTheme) {
+                radioLight.isChecked = true
+            } else {
+                radioDark.isChecked = true
+            }
+
+            radioGroup.setOnCheckedChangeListener { _, checkedId ->
+                when (checkedId) {
+                    R.id.radioLight -> {
+                        currentTheme = R.style.AppTheme_LightTheme
+                        setTheme(currentTheme)
+                        recreate() // Reinicie a atividade para aplicar o novo tema
+                    }
+                    R.id.radioDark -> {
+                        currentTheme = R.style.AppTheme_DarkTheme
+                        setTheme(currentTheme)
+                        recreate() // Reinicie a atividade para aplicar o novo tema
+                    }
+                }
+                myDialog.dismiss()
+            }
 
             myDialog.setCancelable(true)
             myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             myDialog.show()
-
-            val SairSobre = dilaogBinding.findViewById<Button>(R.id.dialog_Sair)
-
-            SairSobre.setOnClickListener()
-            {
-                myDialog.dismiss()
-            }
         }
-
-
-
-
-
     }
 }
