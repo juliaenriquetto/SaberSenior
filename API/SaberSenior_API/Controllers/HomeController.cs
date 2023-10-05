abf91f1b25856ddbec858670843c3b0b7c61d3e2
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2023-10-05 19:36:14 -0300

    retrofit login

--- API/SaberSenior_API/Controllers/HomeController.cs
+++ API/SaberSenior_API/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Mvc;
 
 namespace SaberSenior_API.AddControllers
 {
--- app/src/main/java/com/example/sabersenior/Login.kt
+++ app/src/main/java/com/example/sabersenior/Login.kt
@@ -4,12 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Login : AppCompatActivity() {
     lateinit var edtNome: EditText
     lateinit var edtTelefone: EditText
     lateinit var edtFraseSecreta: EditText
     lateinit var btnLogin: EditText
+
+    fun login(){
+        val retrofitClient = RetrofitConfig.getRetrofit()
+        val service = retrofitClient.create(Service::class.java)
+        val callback = service.incluirUsuario(edtNome.text.toString(), edtTelefone.text.toString(), edtFraseSecreta.text.toString())
+
+        callback.enqueue(object : Callback<Usuario> {
+            override fun onResponse(call: Call<Usuario>?, response: Response<Usuario>?) {
+                Toast.makeText(baseContext, "Nome: ${response!!.body()?.nome}", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onFailure(call: Call<Usuario>?, t: Throwable?) {
+                println("Erro ao realizar login")
+            }
+
+        })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +40,18 @@ class Login : AppCompatActivity() {
         edtTelefone = findViewById(R.id.edtTelefone)
         edtFraseSecreta = findViewById(R.id.edtFraseSecreta)
 
-        btnLogin.setOnClickListener{
+        btnLogin.setOnClickListener{login()}
 
-            val idFraseSecreta = edtFraseSecreta.text.toString()
-            val nome = edtNome.text.toString()
-            val telefone = edtTelefone.text.toString()
+        /*
+        val idFraseSecreta = edtFraseSecreta.text.toString()
+        val nome = edtNome.text.toString()
+        val telefone = edtTelefone.text.toString()
 
 
-            var usuario = Usuario(15, idFraseSecreta, nome, telefone)
-            val intent = Intent(this, TelaJogos::class.java)
-            startActivity(intent)
-        }
+        var usuario = Usuario(15, idFraseSecreta, nome, telefone)
+        val intent = Intent(this, TelaJogos::class.java)
+        startActivity(intent)
+
+      */
     }
 }
