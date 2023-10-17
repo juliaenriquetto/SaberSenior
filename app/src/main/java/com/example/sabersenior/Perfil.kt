500c67d696ab3aa433aaa247e57df7c4db049aa5
Laura Nakamura <112010242+Lauranakamura@users.noreply.github.com>
2023-10-17 17:06:37 -0300

    oiii

--- app/src/main/java/com/example/sabersenior/Perfil.kt
+++ app/src/main/java/com/example/sabersenior/Perfil.kt
@@ -15,42 +15,8 @@ class Perfil : AppCompatActivity() {
         setContentView(R.layout.activity_perfil)
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
         imageView2.setImageResource(R.drawable.imagemteste)
-
-        val btnedtPerfil = findViewById<Button>(R.id.btnEditarPerfil) // Adicione essa linha
-
-        btnedtPerfil.setOnClickListener {
-            showCustomDialog()
-        }
     }
 
-    private fun showCustomDialog() {
-        val dialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
-
-        val builder = .Builder(this)
-        builder.setView(dialogView)
-
-        val alertDialog = builder.create()
-
-        val nomeEditText = dialogView.findViewById<editText>(R.id.nomeEditText)
-        val telefoneEditText = dialogView.findViewById<EditText>(R.id.telefoneEditText)
-        val salvarButton = dialogView.findViewById<Button>(R.id.salvarButton)
-        val fecharButton = dialogView.findViewById<Button>(R.id.fecharButton)
-
-        salvarButton.setOnClickListener {
-            val novoNome = nomeEditText.text.toString()
-            val novoTelefone = telefoneEditText.text.toString()
-
-            // Aqui você pode adicionar o código para salvar os dados.
-
-            alertDialog.dismiss()
-        }
-
-        fecharButton.setOnClickListener {
-            alertDialog.dismiss()
-        }
-
-        alertDialog.show()
-    }
 }
 
-    }
+
