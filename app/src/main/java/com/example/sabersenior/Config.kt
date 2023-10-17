98e71ade4783258264d8a9719b73f90978c84439
Laura Nakamura <112010242+Lauranakamura@users.noreply.github.com>
2023-10-17 20:00:11 -0300

    Editar Perfil FEITINHO

--- app/src/main/java/com/example/sabersenior/Config.kt
+++ app/src/main/java/com/example/sabersenior/Config.kt
@@ -15,7 +15,6 @@ class Config : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(currentTheme)
         setContentView(R.layout.activity_config)
 
         // Alert Dialog Btn_SobreNos
@@ -23,37 +22,9 @@ class Config : AppCompatActivity() {
 
         btnSobreNos.setOnClickListener {
             val dialogBinding = layoutInflater.inflate(R.layout.custom_allert_sobre_nos, null)
-            val RadiusP = layoutInflater.inflate(R.layout.dialog_mudtema, null)
 
             val myDialog = Dialog(this)
             myDialog.setContentView(dialogBinding)
-
-            val radioGroup = RadiusP.findViewById<RadioGroup>(R.id.radioGroup)
-            val radioLight = RadiusP.findViewById<RadioButton>(R.id.radioLight)
-            val radioDark = RadiusP.findViewById<RadioButton>(R.id.radioDark)
-
-            // Configure os RadioButtons para refletir o tema atual
-            if (currentTheme == R.style.AppTheme_LightTheme) {
-                radioLight.isChecked = true
-            } else {
-                radioDark.isChecked = true
-            }
-
-            radioGroup.setOnCheckedChangeListener { _, checkedId ->
-                when (checkedId) {
-                    R.id.radioLight -> {
-                        currentTheme = R.style.AppTheme_LightTheme
-                        setTheme(currentTheme)
-                        recreate() // Reinicie a atividade para aplicar o novo tema
-                    }
-                    R.id.radioDark -> {
-                        currentTheme = R.style.AppTheme_DarkTheme
-                        setTheme(currentTheme)
-                        recreate() // Reinicie a atividade para aplicar o novo tema
-                    }
-                }
-                myDialog.dismiss()
-            }
 
             val SairSobre = dialogBinding.findViewById<Button>(R.id.dialog_Sair)
 
--- app/src/main/java/com/example/sabersenior/Perfil.kt
+++ app/src/main/java/com/example/sabersenior/Perfil.kt
@@ -13,10 +13,29 @@ class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
         imageView2.setImageResource(R.drawable.imagemteste)
+
+        val btnedit = findViewById<Button>(R.id.btnEditarPerfil)
+
+        btnedit.setOnClickListener {
+            val myDialog = Dialog(this)
+            myDialog.setContentView(R.layout.editerfil)
+
+            val Sairedit = myDialog.findViewById<Button>(R.id.editcancelar)
+
+            Sairedit.setOnClickListener {
+                myDialog.dismiss()
+            }
+
+            myDialog.show()
+        }
     }
 
+
 }
+
+
 
 
