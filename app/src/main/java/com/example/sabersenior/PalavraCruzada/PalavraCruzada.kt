eed980e54cb57053c87f4f3ff3c0fcc34965d30e
Clara Andrade <113606213+claraandradee@users.noreply.github.com>
2023-10-31 11:33:54 -0300

    implementacao do jogo - Palavra Cruzada
    obs: está com muito erro

---
+++ app/src/main/java/com/example/sabersenior/PalavraCruzada/PalavraCruzada.kt
@@ -0,0 +1,10 @@
+package com.example.sabersenior.PalavraCruzada
+
+import android.os.Bundle
+
+class PalavraCruzada {
+    protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela__menu)
+    }
+}
